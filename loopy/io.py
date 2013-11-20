04514c5ba0f8cf1599498dc2921b3528b2d77f3d
Jan Paral <jparal@gmail.com>
2013-11-20 13:45:46 -0500

    NEW [io] A new HDF5 load function.
    The function traverse HDF5 files and creates structured dictionary. We assume
    only CArry data types to be stored inside of HDF5.

--- loopy/io.py
+++ loopy/io.py
@@ -1,41 +1,64 @@
 
-import h5py
 import tables as pt
 import numpy as np
+import loopy as lpy
 
-def loadhdf5(fname,variable):
-
-    h5file=h5py.File(fname,'r')
-    data = h5file[variable][...]
-    h5file.close()
-    return data
-
-def writehdf5(fname, name, data, new=False):
+def loadhdf5(fname, path='/'):
     """
-    .. py:function:: writehdf5(fname, name, data, new=False)
+    .. py:function:: writehdf5(fname, path='/')
 
-    The function writes HDF5 file using PyTables with compression.
+    The function traverse HDF5 files and creates structured dictionary. We assume
+    only CArry data types to be stored inside of HDF5.
+
+    :param fname: File name to read.
+    :param path: Root path from where to start reading.
+    :rtype: loopy.struct (i.e. dictionary)
+    """
+    def _traverse_tree(h5f, path):
+        dat = lpy.struct()
+        for node in h5f.listNodes(where=path, classname='CArray'):
+            dat[node._v_name] = node.read()
+
+        for node in h5f.listNodes(where=path, classname='Group'):
+            name = node._v_name
+            dat[name] = _traverse_tree(h5f, path+'/'+name)
+        return dat
+
+    h5f = pt.File(fname, 'r')
+    dat = _traverse_tree(h5f, path)
+    h5f.close()
+    return dat
+
+def writehdf5(fname, data, path='/', append=False):
+    """
+    .. py:function:: writehdf5(fname, data, path='/', append=False)
+
+    The function writes HDF5 file using PyTables and CArray.
+    This is high level function which shoud handle the most common scenarios.
 
     :param fname: name of the HDF5 file
-    :param loc: location inside of HDF5 file (e.g. /new/Bz)
+    :param path: location inside of HDF5 file (e.g. /new/Bz)
     :param data: the actual data to be stored
     :type data: dict or ndarray otherwise will be converted into ndarray
+    :param append: Should the data be appended to an existing file?
     :rtype: none
     """
-    mode = 'w' if new else 'a'
+    mode = 'a' if append else 'w'
     filters = pt.Filters(complevel=6)
     h5f = pt.File(fname, mode)
+
+    # Remove double slashes and the last one
+    path = '/'+'/'.join(filter(None, path.split('/')))
+    dloc = path.rsplit('/',1)
+    root = dloc[0] if np.size(dloc) > 1 else '/'
+    root = root if root.startswith('/') else '/' + root
+    name = path if np.size(dloc) == 1 else dloc[1]
 
     if isinstance(data, dict):
         h5f.close()
         for key in data.keys():
-            writehdf5(fname, name+'/'+key, data[key], new=False)
+            writehdf5(fname, data[key], path=path+'/'+key, append=True)
         return
-
-    path = name.rsplit('/',1)
-    root = path[0] if np.size(path) > 1 else '/'
-    root = root if root.startswith('/') else '/' + root
-    name = name if np.size(path) == 1 else path[1]
 
     if not isinstance(data, np.ndarray):
         data = np.array(data, ndmin=1)
