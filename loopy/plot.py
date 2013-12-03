2b19ae0826d7b51a0a9597e5c744d011d6c29e14
Jan Paral <jparal@gmail.com>
2013-12-03 15:38:40 -0500

    ENH [savefigs] Add 'format' parameter to savefigs.
    The reason is so we dont have to save large eps files all the time.

--- loopy/plot.py
+++ loopy/plot.py
@@ -4,19 +4,30 @@ from matplotlib.pyplot import savefig as _savefig
 
 __all__ = ['savefigs', 'panel_label']
 
-def savefigs(fname, dname='fig'):
+def savefigs(fname, outdir='fig', formats=['jpg', 'png']):
+    """
+    Save thumbnail figure in the current directory of hires in `outdir`.
 
+    :param fname: File name without extension.
+    :param outdir: Output directory of the hi-res figures.
+    :param formats: Requested formats (jpg|png|eps).
+    """
     if not os.path.exists(dname):
         print 'Creating figure directory: ' + dname
         os.makedirs(dname)
 
+    # Thumbnail
     _savefig(fname + '.jpg', dpi=100)
+
     oname = 'fig/' + fname
 
-    _savefig(oname + '.eps', bbox_inches='tight', pad_inches=0)
-    _savefig(oname + '.jpg', dpi=300, bbox_inches='tight', pad_inches=0)
-    _savefig(oname + '.png', dpi=300, bbox_inches='tight', pad_inches=0, \
-            transparent=True)
+    if 'eps' in formats:
+        _savefig(oname + '.eps', bbox_inches='tight', pad_inches=0)
+    if 'jpg' in formats or 'jpeg' in formats:
+        _savefig(oname + '.jpg', dpi=300, bbox_inches='tight', pad_inches=0)
+    if 'png' in formats:
+        _savefig(oname + '.png', dpi=300, bbox_inches='tight', pad_inches=0, \
+                 transparent=True)
 
 def panel_label(ax, label):
     #ydiff = ax.transAxes.transform((0,1))-ax.transAxes.transform((0,0))
